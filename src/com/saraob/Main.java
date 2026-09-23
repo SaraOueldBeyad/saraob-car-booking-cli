2221e66b79c027b3e6dce66ef6b7d409a931bf1c
Sara Oueld Beyad <oueldbeyad.sara@gmail.com>
2026-09-23 17:30:06 -0400

    feat: implement car booking cli system

--- src/com/saraob/Main.java
+++ src/com/saraob/Main.java
@@ -16,7 +16,9 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scnr = new Scanner(System.in);
+
         CarBookingService carBookingService = new CarBookingService();
+
         int choice = 0;
 
         do {
